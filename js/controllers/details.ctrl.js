2620d5fd949969718745964a0f633b5bda9aa6ff
Nignesh <NiGi@Nigneshs-MacBook-Pro.local>
2015-10-23 22:15:51 -0400

    code formatting

--- js/controllers/details.ctrl.js
+++ js/controllers/details.ctrl.js
@@ -1,24 +1,21 @@
 angular.module('Booya')
-.controller('DetailsCtrl', function($scope, $routeParams, Items){
-	
-	$scope.itemId = $routeParams.itemID;
-	$scope.item = {};
-	
-	//$http({method:'GET', url:'data/items.json'})
-	Items.getOne($scope.itemId)
-    .then(function(response){
-        //success
-        var items = response.data.items;
-		items.forEach(function(i){
-			if(i.id == $routeParams.itemID){
-				$scope.item = i;
-			}
-		});
-    }, function(response){
-        //error
-        
-    });
-	
-   
-    
-});
+	.controller('DetailsCtrl', function ($scope, $routeParams, Items) {
+
+		$scope.itemId = $routeParams.itemID;
+		$scope.item = {};
+
+		//$http({method:'GET', url:'data/items.json'})
+		Items.getOne($scope.itemId)
+			.then(function (response) {
+				//success
+				var items = response.data.items;
+				items.forEach(function (i) {
+					if (i.id == $routeParams.itemID) {
+						$scope.item = i;
+					}
+				});
+			}, function (response) {
+				//error
+
+			});
+	});
